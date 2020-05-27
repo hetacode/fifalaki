8472a0dbab9d9892cf249bff5cb26d2949030616
Seba G <sg@hetacode.pl>
2020-05-27 23:36:55 +0200

    fixed bind queue with exchange

--- src/Rtm/services/bus.service.ts
+++ src/Rtm/services/bus.service.ts
@@ -3,15 +3,21 @@ import amqp from "amqplib"
 export const busService = async () => {
     let con = await amqp.connect(process.env.RABBIT_SERVER as string)
     let ch = await con.createChannel()
-    await ch.assertExchange("game-ex", "fanout")
+    await ch.assertExchange("rtm-ex", "fanout", {
+        durable: false,
+        autoDelete: false
+    })
+
     await ch.assertQueue("rtm", {
         durable: false,
         autoDelete: false,
         exclusive: false
     })
 
+    await ch.bindQueue("rtm", "rtm-ex", "")
+
     ch.consume("rtm", (msg) => {
-        console.log(`Event: ${msg?.content.toLocaleString()}`)
+        console.log(`Event: ${msg?.content?.toLocaleString()}`)
         ch.ack(msg!, true)
     })
 }
