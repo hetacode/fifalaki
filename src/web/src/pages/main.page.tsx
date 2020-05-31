7f695d0deb1d7fb48d136fc42e095608cd4b710d
Seba G <sg@hetacode.pl>
2020-05-31 21:26:14 +0200

    added env files and main page

---
+++ src/web/src/pages/main.page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const MainPage = (props: Props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default MainPage
