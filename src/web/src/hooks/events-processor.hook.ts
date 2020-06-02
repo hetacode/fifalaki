5d0c2466ebd767dc3a2e04a6e2ecf4596ce19f4d
Seba G <sg@hetacode.pl>
2020-06-02 23:53:47 +0200

    added special hook as events processor

---
+++ src/web/src/hooks/events-processor.hook.ts
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+import { gameState } from "../states/game.state";
+import { useSetRecoilState } from "recoil";
+import { GameStateEnum } from "../enums/game-state.enum";
+
+export function useEventsProcessor() {
+    const setGameState = useSetRecoilState(gameState)
+
+    const process = (e: any) => {
+        switch (e.Type) {
+            case "UpdatedPlayers":
+                setGameState(s => { return { ...s, players: e.Players } })
+                break;
+            case "WaitingForNextLevel":
+                setGameState(s => { return { ...s, state: GameStateEnum.WaitingForLevel, maxTime: e.StateTime } })
+                break;
+            case "NewLevel":
+                setGameState(s => { return { ...s, letters: e.Letters, answers: e.Answers } })
+                break;
+            case "SummaryLevel":
+                setGameState(s => { return { ...s, points: e.Points, attempts: e.attempts, winnerId: e.WinnerId, looserId: e.LooserId } })
+                break;
+            case "EndGame":
+                setGameState(s => { return { ...s, points: e.Points, winnerId: e.WinnerId } })
+                break;
+        }
+
+    }
+
+    return process
+}
--- src/web/src/pages/root.tsx
+++ src/web/src/pages/root.tsx
@@ -3,6 +3,7 @@ import io from "socket.io-client"
 import PlayerGamePage from './PlayerGamePage/player-game.page'
 import { useSetRecoilState } from 'recoil'
 import { appState, IAppState } from '../states/app.state'
+import { useEventsProcessor } from '../hooks/events-processor.hook'
 
 interface Props {
 
@@ -10,12 +11,15 @@ interface Props {
 
 const Root = (props: Props) => {
     const setAppState = useSetRecoilState(appState)
+    const processEvents = useEventsProcessor()
+    
 
     useEffect(() => {
         let socket = io(`${process.env.REACT_APP_RTM_ENDPOINT}`)
         console.log(socket.id)
         socket.on("event", (e: any) => {
             console.log(e)
+            processEvents(e)
         })
         socket.on("connect", () => {
             console.log(`Connection: ${socket.id}`)
