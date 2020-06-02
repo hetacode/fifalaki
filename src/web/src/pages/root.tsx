c511df697c600a19323a7d8e5e7df8a73fc5a37c
Seba G <sg@hetacode.pl>
2020-06-02 23:38:54 +0200

    save connection id to store

--- src/web/src/pages/root.tsx
+++ src/web/src/pages/root.tsx
@@ -1,12 +1,16 @@
 import React, { useEffect } from 'react'
 import io from "socket.io-client"
 import PlayerGamePage from './PlayerGamePage/player-game.page'
+import { useSetRecoilState } from 'recoil'
+import { appState, IAppState } from '../states/app.state'
 
 interface Props {
 
 }
 
 const Root = (props: Props) => {
+    const setAppState = useSetRecoilState(appState)
+
     useEffect(() => {
         let socket = io(`${process.env.REACT_APP_RTM_ENDPOINT}`)
         console.log(socket.id)
@@ -14,7 +18,8 @@ const Root = (props: Props) => {
             console.log(e)
         })
         socket.on("connect", () => {
-            console.log(socket.id)
+            console.log(`Connection: ${socket.id}`)
+            setAppState((s: IAppState) => { return { ...s, connectionId: socket.id } })
         })
     }, [])
     return (
