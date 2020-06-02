bcbe5ec8906ca95c62c44f8f433fe771e56df8d2
Seba G <sg@hetacode.pl>
2020-06-02 22:25:35 +0200

    added player game page

---
+++ src/web/src/pages/PlayerGamePage/player-game.page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const PlayerGamePage = (props: Props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default PlayerGamePage
